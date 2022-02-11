218e59332f8e6c69e0d5c3adc8e52f3a4b8290d6
Kanak <kanakyadav406@gmail.com>
2022-02-11 15:32:06 +0530

    First commit to public repository

---
+++ README.md
@@ -0,0 +1,2 @@
+# FyndFullStackCourse
+# FyndFullStackCourse
---
+++ script.js
@@ -0,0 +1,26 @@
+var my_name = "Kanak";
+var my_num = 9;
+console.log(my_name);
+console.log(my_num);
+
+var num1 = 10;
+num1 += 5;
+
+var num2 = 10;
+num2 -= 5;
+
+var num3 = 10;
+num3 *= 5;
+
+var num4 = 10;
+num4 /= 5;
+
+var num5 = 10;
+num5 %= 5;
+
+
+console.log(num1);
+console.log(num2);
+console.log(num3);
+console.log(num4);
+console.log(num5);
